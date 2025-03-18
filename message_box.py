5e5984e088579b99f78d9b6ff4359656c0345c63
dhinesh63794 <144306459+dhinesh63794@users.noreply.github.com>
2025-03-18 14:29:07 +0530

    Add files via upload

---
+++ message_box.py
@@ -0,0 +1,45 @@
+from tkinter import Tk
+from tkinter  import messagebox
+main=Tk()
+messagebox.showinfo("welcome","welcome to the application explore the feature and get started!")
+main.mainloop()
+
+
+from tkinter import Tk
+from tkinter  import messagebox
+main=Tk()
+messagebox.showwarning("warning","you have unsaved progress the page will result in losing change!")
+main.mainloop()
+
+
+from tkinter import Tk
+from tkinter  import messagebox
+main=Tk()
+messagebox.askquestion("delete confirmation","Are you sure want to delete this item permanently!")
+main.mainloop()
+
+
+main.mainloop()
+
+
+from tkinter import Tk
+from tkinter  import messagebox
+main=Tk()
+messagebox.askretrycancel("Retry","Refresh your page!")
+main.mainloop()
+
+
+from tkinter import Tk
+from tkinter  import messagebox
+main=Tk()
+messagebox.askokcancel("last page","Because not avaliable this page!")
+main.mainloop()
+
+
+from tkinter import Tk
+from tkinter  import messagebox
+main=Tk()
+messagebox.askretrycancel("hello customer","Thanks for watching this!")
+messagebox.askokcancel("last page","because not avaliable this page!")
+messagebox.showinfo("welcome","welcome to the application explore the feature and get started!")
+main.mainloop()
